be97775ad32860a1be1e53912f645e2477123629
Colter Hanna <cerisa@strangerelics.com>
2016-01-06 15:55:20 -0800

    move typedefs to graphics.h

--- code/win_graphics.cpp
+++ code/win_graphics.cpp
@@ -4,21 +4,12 @@
 #include <stdio.h>
 #include <stdint.h>
 
-typedef int8_t int8;
-typedef int16_t int16;
-typedef int32_t int32;
-typedef int64_t int64;
-
-typedef int32 bool32;
-
-typedef uint8_t uint8;
-typedef uint16_t uint16;
-typedef uint32_t uint32;
-typedef uint64_t uint64;
-
-#include "win_graphics.h"
 #include "graphics.h"
 #include "graphics.cpp"
+#include "win_graphics.h"
+
+
+
 
 internal void
 Win_Write8(WIN_GIF_WRITER *writer, uint8 byte)
